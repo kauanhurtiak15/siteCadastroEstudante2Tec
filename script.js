046917aec6a75efa216db07e34fd0a2b86cc64a9
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2026-05-04 17:42:43 +0000

    OK

---
+++ script.js
@@ -0,0 +1,17 @@
+let form = document.getElementById('cadastroForm');
+
+let lista = document.getElementById('listaAlunos');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault()
+
+    const nome = document.getElementById('nomeAluno').value;
+    const idade = document.getElementById('idadeAluno').value;
+
+    const novoItem = document.createElement('li');
+    novoItem.innerHTML = `<strong> ${nome} </strong> - ${idade} anos`;
+
+    lista.appendChild(novoItem);
+
+    form.reset();
+});
